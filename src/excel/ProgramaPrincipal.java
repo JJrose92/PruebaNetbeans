2c7376286a6878a2c2e10029433631d9338bdb27
JJrose92 <juan-sa92@hotmail.com>
2018-07-03 19:10:29 +0200

    Errores corregidos

---
+++ src/excel/ProgramaPrincipal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excel;
+
+
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
+
+/**
+ *
+ * @author Juan
+ */
+public class ProgramaPrincipal {
+    
+        public static void main(String[] args) throws IOException, InvalidFormatException {
+        
+        ProgramasExcel pex = new ProgramasExcel();
+            
+
+        pex.comprobarExcel();
+
+
+        
+        
+    }  
+    
+    
+}
